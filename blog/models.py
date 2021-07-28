c2248ac984a42635a206ab9f448b0880feabddf4
조성범 <joseongbeom@joseongbeom-ui-MacBookPro.local>
2021-07-28 17:20:42 +0900

    다시 커밋

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,6 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
 
-
 class Post(models.Model):
     title = models.CharField(max_length=50)
     hook_text = models.CharField(max_length=100, blank=True)
@@ -29,11 +28,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
-
-
     def __str__(self):
         return f'[{self.pk}] {self.title} :: {self.author}'
-
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
